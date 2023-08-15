032dd9a7096630ecf8ccdc2a8ea69a987d8a4ea7
admin <407374@qq.com>
2023-08-16 06:54:34 +0800

    更新

--- api/api.js
+++ api/api.js
@@ -6,6 +6,9 @@ const path = require("path");
 const publicFilePath=path.join(__dirname, 'public');
 app.use('/', express.static(publicFilePath))
 
+const SQLite3 = require('sqlite3').verbose();
+// 创建一个数据库连接
+const db = new SQLite3.Database(publicFilePath+'/database.db');
 
 //设置允许跨域访问该服务.
 app.all('*', function (req, res, next) {
@@ -14,6 +17,32 @@ app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Methods', '*');
     // res.header("Pragma", "no-cache");
     next();
+});
+
+app.get("/test", (req, res) => {
+
+
+    db.all('select * from MNLG_STU_INFO where name=?',[],function (err,rows) {
+        res.send(rows);
+    })
+
+});
+
+app.get("/search", (req, res) => {
+
+    console.log(req.query.q)
+    db.all('select * from MNLG_STU_INFO where STU_NAME = ?',[req.query.q],function (err,rows) {
+        console.log(rows);
+        for (let row of rows) {
+            res.send({
+                title:row.STU_NAME,
+                text:`身份证:${row.STU_CARDNO} 性别${row.STU_SEX} 联系电话${row.STU_PHONE}`,
+            });
+        }
+
+
+    })
+
 });
 
 app.get("/path", (req, res) => {
